c96e611c8c640c1e9c23f93bbe5a9d2217a1b2c3
jagpreetrahi <jagpreet1615@gmail.com>
2025-04-24 21:34:28 +0530

    Get the sample template from other project

---
+++ src/utils/common/enum-data-types.js
@@ -0,0 +1,11 @@
+const Seat_types = {
+    BUSINESS : 'BUSINESS',
+    ECONOMY : 'ECONOMY',
+    FIRST_CLASS : 'FIRST CLASS',
+    PREMIUM_ECONOMY : 'PREMIUM ECONOMY'
+
+}
+
+module.exports = {
+    Seat_types
+}
