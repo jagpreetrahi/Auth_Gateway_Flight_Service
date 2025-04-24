07eafdff53be242dbe3e18adf6870b88e7fce0f7
jagpreetrahi <jagpreet1615@gmail.com>
2025-04-24 23:23:49 +0530

    Added the custom function for authenticate

---
+++ src/utils/common/auth.js
@@ -0,0 +1,40 @@
+const bcrypt = require('bcryptjs')
+const {ServerConfig} = require('../../config')
+const jwt = require('jsonwebtoken')
+
+function comparePassword(previousPassword , hashedPassword){
+
+    try {
+        return bcrypt.compareSync(previousPassword , hashedPassword);
+    
+    } catch (error) {
+        throw error;
+    }
+} 
+
+function creatToken(inputData){
+    try {
+        const token = jwt.sign(inputData , ServerConfig.JWT_SECRET, {expiresIn : ServerConfig.JWT_EXPIRY})
+        return token
+    } catch (error) {
+        throw error
+    }
+}
+
+function verifyToken(token){
+    try {
+        const verify = jwt.verify(token, ServerConfig.JWT_SECRET);
+        return verify;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+
+
+module.exports = {
+    comparePassword,
+    verifyToken,
+    creatToken
+}
